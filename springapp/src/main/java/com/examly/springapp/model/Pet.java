53986f0951cb2b96af5b976d5721383ded845351
neojarvis <techadmin@iamneo.ai>
2025-09-19 06:25:10 +0000

    Auto-save-commit-9/19/2025--11:54:43-AM

--- springapp/src/main/java/com/examly/springapp/model/Pet.java
+++ springapp/src/main/java/com/examly/springapp/model/Pet.java
@@ -1,8 +1,83 @@
 package com.examly.springapp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Pet {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String species;
+    private String breed;
+    private int age;
+    private String description;
+    private String imageUrl;
+    private String adoptionStatus;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSpecies() {
+        return species;
+    }
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+    public String getBreed() {
+        return breed;
+    }
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+    public String getAdoptionStatus() {
+        return adoptionStatus;
+    }
+    public void setAdoptionStatus(String adoptionStatus) {
+        this.adoptionStatus = adoptionStatus;
+    }
+    public Pet(int id, String name, String species, String breed, int age, String description, String imageUrl,
+            String adoptionStatus) {
+        this.id = id;
+        this.name = name;
+        this.species = species;
+        this.breed = breed;
+        this.age = age;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.adoptionStatus = adoptionStatus;
+    }
+    public Pet() {
+    }
+    
+    
 }
