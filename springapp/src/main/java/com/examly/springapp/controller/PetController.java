000cf40ebef0d4027df9e26c174c6da4d33e5b6e
neojarvis <techadmin@iamneo.ai>
2025-09-19 08:41:46 +0000

    Auto-save-commit-9/19/2025--2:11:19-PM

--- springapp/src/main/java/com/examly/springapp/controller/PetController.java
+++ springapp/src/main/java/com/examly/springapp/controller/PetController.java
@@ -1,8 +1,13 @@
 package com.examly.springapp.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PetController {
 
+    @PostMapping("/api/pets")
+    public Pet postpet(){
+
+    }
 }
