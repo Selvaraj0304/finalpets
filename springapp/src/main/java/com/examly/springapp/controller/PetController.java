f172c2a104cc01a27e0e203347ef95a0fcb67b05
neojarvis <techadmin@iamneo.ai>
2025-09-22 04:59:41 +0000

    Submit-project-save-commit-9/22/2025--10:29:40-AM

--- springapp/src/main/java/com/examly/springapp/controller/PetController.java
+++ springapp/src/main/java/com/examly/springapp/controller/PetController.java
@@ -1,10 +1,14 @@
 package com.examly.springapp.controller;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +38,15 @@ public class PetController {
         return petService.getAllPets();
     }
     @GetMapping("/api/pets/{id}")
-    public Optional<Pet> getById(@PathVariable Long id){
-        return petService.getPetById(id);
+    public ResponseEntity<?> getById(@PathVariable Long id){
+        Optional<Pet> petdetail=petService.getPetById(id);
+        if(petdetail.isPresent()){
+            return ResponseEntity.ok(petdetail);
+        }
+        else{
+            Map<String,String> err=new HashMap<>();
+            err.put("message","Pet with ID "+id+" not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+        }
     }
 }
