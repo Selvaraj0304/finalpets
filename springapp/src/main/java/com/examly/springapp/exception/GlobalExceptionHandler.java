6091b155c92aba7835e67c32439538d38e46c9d5
neojarvis <techadmin@iamneo.ai>
2025-09-22 06:36:07 +0000

    Auto-save-commit-9/22/2025--12:06:07-PM

--- springapp/src/main/java/com/examly/springapp/exception/GlobalExceptionHandler.java
+++ springapp/src/main/java/com/examly/springapp/exception/GlobalExceptionHandler.java
@@ -1,11 +1,23 @@
 package com.examly.springapp.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handler
+    public ResponseEntity<?> validationerrors(MethodArgumentNotValidException ex){
+        Map<String,Object> err=new HashMap<>();
+        err.put("message","validation failed");
+        err.put("errors",ex.getBindingResult().getFieldErrors().stream().map(
+            error->error.getField()+":"+error.getDefaultMessage()
+        ).toList();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
 }
