e2a54a96d75b48e9361d72f60532b679353351a3
neojarvis <techadmin@iamneo.ai>
2025-09-24 06:04:59 +0000

    Auto-save-commit-10/24/2025--11:34:42-AM

--- springapp/src/main/java/com/examly/springapp/PetAdoptionManagementSystemApplication.java
+++ springapp/src/main/java/com/examly/springapp/PetAdoptionManagementSystemApplication.java
@@ -9,7 +9,7 @@ public class PetAdoptionManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetAdoptionManagementSystemApplication.class, args);
-		System.out.println();
+		System.out.println(new BCry);
 	}
 
 }
