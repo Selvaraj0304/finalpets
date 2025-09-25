3f44651df04a02664c66f95af9f7e9746308bf98
neojarvis <techadmin@iamneo.ai>
2025-09-25 17:43:11 +0000

    Submit-project-save-commit-9/25/2025--11:13:10-PM

--- springapp/src/main/java/com/examly/springapp/controller/PetController.java
+++ springapp/src/main/java/com/examly/springapp/controller/PetController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import com.examly.springapp.service.PetService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "https://8081-ccfbdecabcaae334371528adaaadfdebeathree.premiumproject.examly.io")
 public class PetController {
 
     @Autowired
