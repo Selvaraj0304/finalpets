f5fe1d13b177924a67b70e33ef249575182655ac
neojarvis <techadmin@iamneo.ai>
2025-09-25 09:54:54 +0000

    Auto-save-commit-9/25/2025--3:24:53-PM

--- reactapp/src/Parent.js
+++ reactapp/src/Parent.js
@@ -2,31 +2,32 @@ import React, { useState } from 'react'
 import Child from './Child'
 
 const Parent = () => {
-    const [value,setValue]=useState(10);
+    const [value1,setValue]=useState(10);
+    const [value2,setValue2]=useState(10);
+
     function add(){
-      setValue(value+2);
+      setValue(value1+2);
       document.body.style.backgroundColor="red";
       document.getElementById("txt").style.color="white";
     }
     function reduce(){
-      setValue(value-2);
+      setValue2(value2-2);
       document.body.style.backgroundColor="white";
       document.getElementById("txt").style.color="black";
-
     }
-    
     useEffect(()=>{
-
+      console.log("Hello from No dependency");
     });
     useEffect(()=>{
-
+      console.log("Hello from Empty dependency");
     },[]);
     useEffect(()=>{
-
-    },[value]);
+      console.log("Hello from Specific dependency");
+    },[value1]);
   return (
     <div>
-       <h1 id="txt">The number is: {value}</h1>
+      <h1 id="txt">The value2 is : {value2}</h1>
+       <h1 id="txt">The number is: {value1}</h1>
        <button onClick={add}>Click to add</button>
        <button onClick={reduce}>Click to reduce</button>
     </div>
