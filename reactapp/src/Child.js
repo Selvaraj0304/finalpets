9dfd24878b844bd04825adb8b66ac72331d7b805
neojarvis <techadmin@iamneo.ai>
2025-09-25 06:23:46 +0000

    Auto-save-commit-9/25/2025--11:53:43-AM

---
+++ reactapp/src/Child.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Child = () => {
+  return (
+    <div>
+        <h1>Name: </h1>
+        <h3>Price: </h3>
+        <h3>Quantity: </h3>
+    </div>
+  )
+}
+
+export default Child
