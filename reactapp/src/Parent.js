fd43bfcd790fdbad1bdfef7e9040063e0df66d03
neojarvis <techadmin@iamneo.ai>
2025-09-25 08:23:48 +0000

    Auto-save-commit-9/25/2025--1:53:43-PM

--- reactapp/src/Parent.js
+++ reactapp/src/Parent.js
@@ -2,10 +2,21 @@ import React, { useState } from 'react'
 import Child from './Child'
 
 const Parent = () => {
-    const [value,seValue]=useState(10);
+    const [value,setValue]=useState(10);
+    function add(){
+      setValue(value+2);
+      document.body.style.backgroundColor="red";
+    }
+    function reduce(){
+      setValue(value-2);
+      document.body.style.backgroundColor="white";
+
+    }
   return (
     <div>
        <h1>The number is: {value}</h1>
+       <button onClick={add}>Click to add</button>
+       <button onClick={reduce}>Click to reduce</button>
     </div>
   )
 }
