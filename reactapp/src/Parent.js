166c67de87f82da9552365306d691f0fc6fed73a
neojarvis <techadmin@iamneo.ai>
2025-09-25 08:38:46 +0000

    Auto-save-commit-9/25/2025--2:08:45-PM

--- reactapp/src/Parent.js
+++ reactapp/src/Parent.js
@@ -6,15 +6,17 @@ const Parent = () => {
     function add(){
       setValue(value+2);
       document.body.style.backgroundColor="red";
+      document.getElementById("txt").style.color="white";
     }
     function reduce(){
       setValue(value-2);
       document.body.style.backgroundColor="white";
+      document.getElementById("txt").style.color="black";
 
     }
   return (
     <div>
-       <h1>The number is: {value}</h1>
+       <h1 id="txt">The number is: {value}</h1>
        <button onClick={add}>Click to add</button>
        <button onClick={reduce}>Click to reduce</button>
     </div>
