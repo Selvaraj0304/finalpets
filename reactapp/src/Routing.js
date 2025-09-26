96e3067e2b8f8a4ccdbecccacf059703765c4b16
neojarvis <techadmin@iamneo.ai>
2025-09-26 04:29:19 +0000

    testcases

---
+++ reactapp/src/Routing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Routing = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Routing
---
+++ reactapp/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <h1>Hello this is login</h1>
+  )
+}
+
+export default Login
